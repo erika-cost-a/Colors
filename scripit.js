6a81129c5cd35eb6a2464b1c53152e7fd8b1d306
erika-cost-a <94139192+erika-cost-a@users.noreply.github.com>
2021-12-16 16:56:20 -0300

    Delete scripit.js

--- scripit.js
+++
@@ -1,8 +0,0 @@
-let color = document.body
-let blue = document.getElementById('blue')
-
-
-color.addEventListener('click', function () {
-    blue.classList.toggle('blue')
-    color.classList.toggle('bg')
-})
